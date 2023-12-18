0759e5b4abe4c57005cf23c528f9005c22c7b0d5
Greg Cherry <Gcherry2@gmail.com>
2023-12-18 17:28:56 -0500

    add file based routing

---
+++ frontApp/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Touchable, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'
+
+const Page = () => {
+    return (
+        <View>
+            <Text>Front App Begins</Text>
+        </View>
+    );
+};
+
+export default Page;
