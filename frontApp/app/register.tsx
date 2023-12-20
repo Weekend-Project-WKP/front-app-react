2cebdbb9644f0af3177dc85ddb309dcdbda6b4f1
Greg Cherry <Gcherry2@gmail.com>
2023-12-20 08:00:55 -0500

    pr comment updates

--- frontApp/app/register.tsx
+++ frontApp/app/register.tsx
@@ -1,7 +1,7 @@
 import { View, Text, Touchable, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
     return (
