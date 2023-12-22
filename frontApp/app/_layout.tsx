3f9accc62a86493eb30db5aea38859b5ee570574
Greg Cherry <Gcherry2@gmail.com>
2023-12-22 18:15:10 -0500

    Add login page styling

--- frontApp/app/_layout.tsx
+++ frontApp/app/_layout.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { Stack } from 'expo-router';
 
 const Layout = () => {
-    return <Stack>
-        <Stack.Screen name='index' options={{ title: 'Login' }} />
+    return <Stack screenOptions={{
+        headerShown: false
+    }}>
+        <Stack.Screen name='index' options={{ title: '' }} />
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
     </Stack>;
 };
