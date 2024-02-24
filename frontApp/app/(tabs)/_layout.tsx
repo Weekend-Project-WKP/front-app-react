e5e534772754e5338228bd653f540bb2667bc1e8
Greg Cherry <Gcherry2@gmail.com>
2024-02-24 15:49:52 -0500

    fix layout

--- frontApp/app/(tabs)/_layout.tsx
+++ frontApp/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { Stack, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons'
 
 const Layout = () => {
